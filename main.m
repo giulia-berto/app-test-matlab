0d235ce8a9d56728b30a15eebaacf6815f3fd6ac
Giulia Berto <gberto@fbk.eu>
2021-02-19 18:11:30 +0100

    recompiled

--- main.m
+++ main.m
@@ -2,10 +2,16 @@
 % Copyright (c) 2021 brainlife.io 
 % Indiana University
 
-% Add submodules
-addpath(genpath(pwd))
+function main()
+
+% Add submodules (HCP systems)
+if ~isdeployed
+    addpath(genpath(pwd))
+end
 
 % Load config.json
 config = loadjson('config.json','ParseStringArray',1); 
 
 disp(config.test)
+
+end
